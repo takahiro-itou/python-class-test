1a8d057d2f6ba726d9d2a0756b655d453e85a1b1
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-01-22 21:11:23 +0900

    テスト

---
+++ test2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class HogeA:
+    def __init__(self, v):
+        print('Init HogeA')
+        self.value = v
+
+    def print_value(self):
+        print("HogeA : value = {0}".format(self.value,))
+
+class FugaB:
+    def __init__(self, v):
+        print('Init FugaB')
+        self.value = v
+
+    def print_value(self):
+        print("FugaB : value = {0}".format(self.value,))
+
+def instantiate(cls, val):
+    return  cls(val)
+
+x1 = instantiate(HogeA, 1234)
+x1.print_value()
+
+x2 = instantiate(FugaB, 555)
+x2.print_value()
