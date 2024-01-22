188bc05025555381392d3cb9a4823e5573bdfafc
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-01-22 21:14:42 +0900

    モジュールを追加

---
+++ mymodules/piyo.py
@@ -0,0 +1,8 @@
+
+class PiyoPiyoC:
+    def __init__(self, v):
+        print('Init PiyoPiyoC')
+        self.value = v
+
+    def print_value(self):
+        print("PiyoPiyoC : value = {0}".format(self.value * 2,))
